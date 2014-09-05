d17ae1d8be1dce6e0543d7320e9796fd01a697d7
Yinan-Zhang <yinan.zhang.gr@dartmouth.edu>
2014-09-05 14:06:43 -0400

    high dimensional ball

--- basics/math/hyper_geometry.py
+++ basics/math/hyper_geometry.py
@@ -15,6 +15,10 @@ __revision__ = '$Revision$'
 
 from math import sqrt, pi, degrees, radians, cos, sin, acos, asin, atan2
 
+
+#========================================================================================
+# vector
+#========================================================================================
 class vec:
 	def __init__(self, coords ):
 		'''@param coords: n-element list'''
@@ -129,7 +133,7 @@ class vec:
 		for i in range(0,len(self.pos)):
 			self[i] /= other;
 		return self;
-		
+
 	def __itruediv__( self, other ):
 		for i in range(0,len(self.pos)):
 			self[i] /= other;
@@ -143,3 +147,26 @@ class vec:
 			result.append( self[i] * (-1));
 		return vec(result);
 
+
+
+#========================================================================================
+# Sphere in high dimension
+#========================================================================================
+class sphere:
+	def __init__( self, center, radius ):
+		''' Open high dimensional sphere
+		@param center: (vec) the center of the sphere
+		@param radius: (number) the radius of the sphere'''
+		self.center = center;
+		self.radius = radius;
+
+	def contains( self, point ):
+		'''determine if the point is inside the sphere
+		@param point: (vec) the point'''
+		dist = (point - self.center).r();
+		return dist < point;
+
+	def intersects( self, other ):
+		'''determine if the other sphere is intersecting with self'''
+		center_dist = (self.center - other.center).r();
+		return center_dist < self.radius + other.radius;
