71b6d210a6f2bd54971f01958704182a71c35ed4
IanZhang1990 <ian.zhang1990@gmail.com>
2014-10-25 15:05:45 -0400

    detail sampling

---
+++ sampling/DetailSampling/detail_sampling.py
@@ -0,0 +1,5 @@
+'''
+Given a set of sphere samples that almost cover the free space. 
+We want to make sure that no 1d hole are generated because of sampling.
+That is we will not increase 1st betti number becuse of no enough samples.
+'''
