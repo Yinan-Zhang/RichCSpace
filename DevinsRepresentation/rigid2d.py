bccfd1606aa28e16cdc2f8aa292f2a4f4635fd32
Yinan-Zhang <yinan.zhang.gr@dartmouth.edu>
2014-11-18 10:28:16 -0500

    classified configs

--- DevinsRepresentation/rigid2d.py
+++ DevinsRepresentation/rigid2d.py
@@ -54,7 +54,12 @@ class Rigid2D:
 		pass;
 
 	def get_points(self):
-		'''Get points of four corners'''
+		'''Get points of four corners.
+			p1	|--| p2
+				|  |
+			p4	|--| p3
+
+		'''
 		A = v2(self.state[0], self.state[1]);
 		B = v2(self.state[2], self.state[3]);
 		hw = self.width/2.0;
@@ -111,13 +116,69 @@ class Rigid2D:
 		B = v2(self.state[2], self.state[3]);
 		return math.fabs( (A-B).r() - self.length ) <= 0.01;
 
-	def get_random_state(self, disp_dist):
+	def get_classified_random_states( self, n, disp_dist ):
+		'''A sphere in C-space for the rigid body is like this:
+			V1	 ^
+				 |
+			 ....|....
+			 .	 |   .
+		-----.---o---.----------> V2
+			 .   |O  .
+			 ....|....
+				 |
+		Where V1 and V2 are two vertices of the rectangle. ( the whole sphere is in 4d space. )
+
+		We want to map configs in the surface of the sphere back to our C-space. 
+		'''
+
+		sphere_samples = [];
+		for i in range(n):
+			sphere_samples.append( self.get_random_state(disp_dist) );
+
+		origin_state = copy.copy(self.state);
+		origin_pnts = self.get_points();
+		P1_group = [];
+		P2_group = [];
+		P3_group = [];
+		P4_group = [];
+
+		for new_state in sphere_samples:
+			self.set_state(new_state);
+			new_pnts = self.get_points();
+			# Find the point that is max dist
+			max_idx = 0;
+			max_dist = 0;
+			for i in range(4):
+				dist = (origin_pnts[i] - new_pnts[i]).r();
+				if dist > max_dist:
+					max_dist = dist;
+					max_idx = i;
+			if max_idx == 0:
+				P1_group.append(new_state);
+			elif max_idx == 1:
+				P2_group.append(new_state);
+			elif max_idx == 2:
+				P3_group.append(new_state);
+			elif max_idx == 3:
+				P4_group.append(new_state);
+
+		self.set_state(origin_state);
+		print "Got {0} samples in p1 group.".format(len(P1_group));
+		print "Got {0} samples in p2 group.".format(len(P2_group));
+		print "Got {0} samples in p3 group.".format(len(P3_group));
+		print "Got {0} samples in p4 group.".format(len(P4_group));
+		return P1_group, P2_group, P3_group, P4_group;
+
+	def __sphere__right__( self ):
+		B = v2(self.state[2], self.state[3]);
+
+	def get_random_state(self, disp_dist, surf = None):
 		'''get a random state that has a fixed displacement-distance to current state.
 		The idea is to have a random state first. Then pull/push the robot until the 
 		displacement-distance is equal to disp_dist'''
 		center  = v2( (self.state[0]+self.state[2])/2.0, (self.state[1]+self.state[3])/2.0)
-		B_ 		= v2( random.randint(center.x-300, center.x+300), random.randint(center.y-300, center.y+300) );		# random point position
-		theta 	= (random.randint(-180, 180) / 360.0) * 2 * math.pi;		# random orientation.
+		B_ 		= v2( random.randint(center.x-500, center.x+500), random.randint(center.y-500, center.y+500) );		# random point position
+		theta 	= (random.randint(0, 360) / 360.0) * 2 * math.pi;		# random orientation.
 		l 		= self.length;
 		A_ 		= v2( B_.x + l * math.cos(theta), B_.y + l * math.sin(theta) );
 		old_state = copy.copy(self.state);
@@ -129,13 +190,16 @@ class Rigid2D:
 		self.set_state(old_state);
 
 		# Find the point that is max dist
+		#pdb.set_trace()
 		max_idx = 0;
 		max_dist = 0;
 		for i in range(4):
 			dist = (old_pnts[i] - new_pnts[i]).r();
+			#print "\t" + str(dist)
 			if dist > max_dist:
 				max_dist = dist;
 				max_idx = i;
+		#print max_idx
 
 		# Get max dist direction.
 		max_dist_dir   = (new_pnts[max_idx] - old_pnts[max_idx])
--- DevinsRepresentation/test.py
+++ DevinsRepresentation/test.py
@@ -33,8 +33,12 @@ def main():
 
 	robot.render(DISPLAYSURF, (255,0,0));
 	disp_dist = 200;
+	
+	'''
+	# These code get random states with disp_dist distance to original one. 
+	# =================================================
 	states = [];
-	for i in range(100):
+	for i in range(10000):
 		rnd_state = robot.get_random_state(disp_dist);
 		if math.fabs(robot.displacement_dist(rnd_state) - disp_dist) <= 0.01:
 			states.append( rnd_state );
@@ -48,13 +52,51 @@ def main():
 		color = (v2(param_state[0], param_state[1]) - v2(origin_param_state[0], origin_param_state[1])).r();
 		param_states.append( (param_state[0],param_state[1],param_state[2],color ) );
 		#param_states.append( (param_state[0],param_state[1],param_state[2]+math.pi*2,color ) );
-		robot.render(DISPLAYSURF, (0, 50 + i * 200/len(states) ,0), 1);
-		#robot.render_head(DISPLAYSURF)
+		#robot.render(DISPLAYSURF, (0, 50 + i * 200/len(states) ,0), 1);
+		robot.render_head(DISPLAYSURF)
 
-	pygame.image.save(DISPLAYSURF, "displacement_dist.PNG");
+	#pygame.image.save(DISPLAYSURF, "displacement_dist.PNG");
+	pygame.image.save(DISPLAYSURF, "head_pos.PNG");
 	save(states, 'cspace.txt');
 	save(param_states, 'param_cspace.txt');
+	'''
 
+	
+	# These code classify previous states into 4 groups
+	# based on vertices of the rigid body.
+	# =================================================
+	p1group, p2group, p3group, p4group = robot.get_classified_random_states(1000, disp_dist);
+	DISPLAYSURF1 = DISPLAYSURF.copy();
+	DISPLAYSURF2 = DISPLAYSURF.copy();
+	DISPLAYSURF3 = DISPLAYSURF.copy();
+	DISPLAYSURF4 = DISPLAYSURF.copy();
+	for state in p1group:
+		robot.set_state(state);
+		robot.render(DISPLAYSURF,  (250,0,0), 1 );
+		robot.render(DISPLAYSURF1, (250,0,0), 1 );
+	for state in p2group:
+		robot.set_state(state);
+		robot.render(DISPLAYSURF,  (0,250,0), 1 );
+		robot.render(DISPLAYSURF2, (0,250,0), 1 );
+	for state in p3group:
+		robot.set_state(state);
+		robot.render(DISPLAYSURF,  (0,0,250), 1 );
+		robot.render(DISPLAYSURF3, (0,0,250), 1 );
+	for state in p4group:
+		robot.set_state(state);
+		robot.render(DISPLAYSURF,  (150,150,150), 1 );
+		robot.render(DISPLAYSURF4, (150,150,150), 1 );
+	
+	pygame.image.save(DISPLAYSURF, "classified_displacement_dist.PNG");
+	pygame.image.save(DISPLAYSURF1, "classified_p1_group.PNG");
+	pygame.image.save(DISPLAYSURF2, "classified_p2_group.PNG");
+	pygame.image.save(DISPLAYSURF3, "classified_p3_group.PNG");
+	pygame.image.save(DISPLAYSURF4, "classified_p4_group.PNG");
+	save(p1group, 'classified_p1_group.txt');
+	save(p2group, 'classified_p2_group.txt');
+	save(p3group, 'classified_p3_group.txt');
+	save(p4group, 'classified_p4_group.txt');
+	
 
 if __name__ == '__main__':
 	main()
